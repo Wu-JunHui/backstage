dd5d7b649b572f8a08b4ba247d02ea1dd1636653
Wu-JunHui <1327480159@qq.com>
2023-02-18 22:35:22 +0800

    deploy

--- src/api/mock.js
+++ src/api/mock.js
@@ -4,23 +4,25 @@ import userApi from './mockData/user.js'
 import permissionApi from './mockData/permission.js'
 // 拦截请求，返回通过Mock模拟的数据
 
-// 本地获取首页图标数据（fastmock）
-Mock.mock('/home/getData', homeApi.getHomeData)
+export default [
+  // 本地获取首页图标数据（fastmock）
+  Mock.mock('/home/getData', homeApi.getHomeData),
 
-// 本地获取user的数据
-Mock.mock(/user\/getUser/, 'get', userApi.getUserList)
+  // 本地获取user的数据
+  Mock.mock(/user\/getUser/, 'get', userApi.getUserList),
 
-// 本地提交新增用户表单接口
-Mock.mock(/user\/add/, 'post', userApi.createUser)
+  // 本地提交新增用户表单接口
+  Mock.mock(/user\/add/, 'post', userApi.createUser),
 
-// 本地提交编辑后的用户信息接口
-Mock.mock(/user\/edit/, 'post', userApi.updateUser)
+  // 本地提交编辑后的用户信息接口
+  Mock.mock(/user\/edit/, 'post', userApi.updateUser),
 
-// 本地调用删除用户接口
-Mock.mock(/user\/delete/, 'get', userApi.deleteUser)
+  // 本地调用删除用户接口
+  Mock.mock(/user\/delete/, 'get', userApi.deleteUser),
 
-// 本地调用登录验证接口
-Mock.mock(/permission\/getMenu/, 'post', permissionApi.getSidebarMenu)
+  // 本地调用登录验证接口
+  Mock.mock(/permission\/getMenu/, 'post', permissionApi.getSidebarMenu)
+]
 
 // export default [
 //   // 本地获取user的数据
