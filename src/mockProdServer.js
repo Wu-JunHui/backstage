4e639e2846d77ab92b9887d85c33a3cb867ba5bb
Wu-JunHui <1327480159@qq.com>
2023-02-18 20:36:31 +0800

    deploy

--- src/mockProdServer.js
+++ src/mockProdServer.js
@@ -1,6 +1,6 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 //这里可以把 mock 文件夹下的所有文件都引入
-import homeModule from './api'
+import homeModule from './api/mockData'
 
 export function setupProdMockServer() {
   createProdMockServer([...homeModule])
